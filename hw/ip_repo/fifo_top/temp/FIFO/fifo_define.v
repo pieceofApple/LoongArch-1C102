ae0063d065049f50015782f7fa07b990c6d1f24a
想成为影之实力者 <116827010+pieceofApple@users.noreply.github.com>
2025-03-06 23:50:31 +0800

    Delete hw directory

--- hw/ip_repo/fifo_top/temp/FIFO/fifo_define.v
+++
@@ -1,7 +0,0 @@
-`define module_name sdram_fifo
-`define getname(oriName,tmodule_name) \~oriName.tmodule_name 
-`define EBR_BASED
-`define Count_W
-`define Count_R
-`define En_Reset
-`define Reset_Synchronization
--- hw/ip_repo/fifo_top/temp/FIFO/fifo_parameter.v
+++
@@ -1,6 +0,0 @@
-parameter WDEPTH = 1024;
-parameter WDSIZE = 16;
-parameter RDEPTH = 1024;
-parameter RDSIZE = 16;
-parameter ASIZE = 10;
-parameter RASIZE = 10;
